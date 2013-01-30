f1d018c3033f0de34e9986d468564f12e5390391
namkyu <lng1982@gmail.com>
2013-01-30 20:09:00 +0900

    테스트 추가

--- src/main/java/com/incross/svc/common/schedule/ScheduleTest.java
+++ src/main/java/com/incross/svc/common/schedule/ScheduleTest.java
@@ -19,12 +19,7 @@ public class ScheduleTest {
 		System.out.println("testSchedule1 스케줄 실행");
 	}
 
-//	@Scheduled(fixedRate = 10000)
-//	public void testSchedule2() {
-//		System.out.println("testSchedule2 스케줄 실행합니다.");
-//	}
-
-	@Scheduled(cron = "0 5 15 * * *")
+	@Scheduled(cron = "${cron.time}")
 	public void testSchedule3() {
 		System.out.println("testSchedule3 스케줄 실행합니다.");
 	}
---
+++ src/test/java/com/incross/svc/component/user/service/UserServiceTest.java
@@ -0,0 +1,23 @@
+package com.incross.svc.component.user.service;
+
+import org.junit.Test;
+
+
+/**
+ * @FileName : UserServiceTest.java
+ * @Project : springProject
+ * @Date : 2013. 1. 29.
+ * @작성자 : 이남규
+ * @프로그램설명 :
+ */
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration("application-context.xml")
+//@ActiveProfiles("dev")
+public class UserServiceTest {
+
+
+	@Test
+	public void postConstructTest() {
+
+	}
+}
